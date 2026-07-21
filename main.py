10e6563e590d81e17b9276bf8b7740161c1ebfd0
Simon Nungwa <simonnungwa99@gmail.com>
2026-07-21 02:34:22 +0100

    Transformer class: constructor and tokenizer stub

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+    Implementing a large language model
+    using real world techniques used for transformer models
+
+"""
+
+class Transformer:
+    def __init__(self, text):
+        self.text = text
+
+    def tokenize(self):
+        ...
